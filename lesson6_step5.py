ea5a3200dbac537799c71deabaea3377c44cddfe
a.tochaev <antonyt@inbox.ru>
2022-04-21 11:39:06 +0300

    Добавил файлы с тестами

---
+++ lesson6_step5.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time 
+import math
+
+link = "http://suninjuly.github.io/find_link_text"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    link = browser.find_element_by_link_text(str(math.ceil(math.pow(math.pi, math.e)*10000)))
+    link.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
